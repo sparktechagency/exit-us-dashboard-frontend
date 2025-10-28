bdeadda076599c7865a2d12b4b97c9965ef1c0bf
jewelrana3 <juyelrana7752@gmail.com>
2025-10-28 09:27:42 +0600

    add live url

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        allowedHosts: ['localhost', '127.0.0.1', 'admin.exoduxz.app'],
+    },
 });
